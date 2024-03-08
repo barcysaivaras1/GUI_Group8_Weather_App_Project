0346fa0c9e73ae71cee36e1afad40e5e358599c3
barcysaivaras1 <barcysaivaras1@gmail.com>
2024-03-08 18:34:21 +0000

    Favourites page
    This adds the html css for the favourites page

---
+++ src/PlusFavouritesButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./PlusButtonStyle.css";
+
+function PlusFavouritesButton(){
+    return(
+        <div className="box">
+            <div className="group">
+                <div className="overlap-group">
+                    <div className="rectangle">
+                        <div className="div"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
