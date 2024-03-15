cc0ef3cc7a6b2d96a899068f24e5d82115c05740
Jack <jackbarry1812@gmail.com>
2024-03-15 20:42:16 +0000

    updating favourites button

--- src/PlusFavouritesButton.jsx
+++ src/PlusFavouritesButton.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import "./PlusButtonStyle.css";
 
-function PlusFavouritesButton(){
-    return(
-        <div className="box">
-            <div className="group">
-                <div className="overlap-group">
-                    <div className="rectangle">
-                        <div className="div"></div>
-                    </div>
-                </div>
-            </div>
+function PlusFavouritesButton() {
+  const handleAddFavorite = () => {
+    const favoritesContainer = document.querySelector(".iphone > .div");
+    const londonEntry = `
+      <div class="favourite-item">
+        <p>
+          <span class="span">London, <br /></span>
+          <span class="text-wrapper-6">United Kingdom</span>
+        </p>
+        <div class="element">12º</div>
+        <img src="img/cloudy.png" alt="Cloudy icon" />
+      </div>
+    `;
+    favoritesContainer.insertAdjacentHTML("beforeend", londonEntry);
+
+    const favoritesCount = document.querySelector(".text-wrapper-5");
+    favoritesCount.textContent = "4 Favourites";
+  };
+
+  return (
+    <div className="box">
+      <div className="group">
+        <div className="overlap-group">
+          <div className="rectangle">
+            <div className="div" onClick={handleAddFavorite}></div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
+
+export default PlusFavouritesButton;
