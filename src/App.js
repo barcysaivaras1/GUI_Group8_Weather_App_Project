26939ecae369ad135e2536a738e505b687f0c916
barcysaivaras1 <barcysaivaras1@gmail.com>
2024-03-17 23:56:17 +0000

    Changed App
    changed app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react';
 import { AppRoute } from './Routes';
 
 const App = () => {
-  const appStyle = {
-    backgroundColor: '#3498db',
-    minHeight: '100vh',  // Ensure the background color covers the entire viewport height
-    padding: '20px',    // Add some padding for better visibility of the content
-    color: 'white',     // Set text color to white for better visibility on the blue background
-  };
+
 
   return(
     <AppRoute/>
