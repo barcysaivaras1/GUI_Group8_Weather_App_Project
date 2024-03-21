7965376f0a020d7e48ef554ddaa731ed6f10ba5f
krishbains <krishbains@gmail.com>
2024-03-21 10:47:28 +0000

    searchPage displays first 25 cities + capital

---
+++ .history/src/components/useCountry_20240321104632.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useCitySelector = () => {
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [cities, setCities] = useState([]);
+  const [capitalCity, setCapitalCity] = useState('');
+  const [temperature, setTemperature] = useState(null);
+  const [cityArray, setCityArray] = useState([]);
+
+  useEffect(() => {
+    // Fetch list of countries when component mounts
+    fetch('https://api.countrystatecity.in/v1/countries', {
+      headers: {
+        'X-CSCAPI-KEY': 'Wmk1Vlhxa3hIVTNBeE9rS3NubE9QSUdheHQ2YmtrQVRPdk1UMERvWg==', // Replace with your actual API key
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        setCountries(data);
+        setFilteredCountries(data); // Initialize filtered countries with all countries
+      });
+  }, []);
+
+  const handleCountryChange = async event => {
+    const countryCode = event.target.value;
+    setSelectedCountry(countryCode);
+  
+    // Fetch cities based on selected country
+    const response = await fetch(`https://api.countrystatecity.in/v1/countries/${countryCode}/cities`, {
+      headers: {
+        'X-CSCAPI-KEY': 'Wmk1Vlhxa3hIVTNBeE9rS3NubE9QSUdheHQ2YmtrQVRPdk1UMERvWg==', // Replace with your actual API key
+      },
+    });
+    const data = await response.json();
+  
+    // Sort cities by population
+    const sortedCities = data.sort((a, b) => b.population - a.population);
+  
+    setCities(sortedCities);
+  
+    // Fetch country information to get capital city
+    const countryResponse = await fetch(`https://api.countrystatecity.in/v1/countries/${countryCode}`, {
+      headers: {
+        'X-CSCAPI-KEY': 'Wmk1Vlhxa3hIVTNBeE9rS3NubE9QSUdheHQ2YmtrQVRPdk1UMERvWg==', // Replace with your actual API key
+      },
+    });
+    const countryData = await countryResponse.json();
+    setCapitalCity(countryData.capital);
+  
+    // Update city array with the capital city and the first four cities
+    const first25Cities = sortedCities.slice(0, 25).map(city => city.name);
+    setCityArray([countryData.capital, ...first25Cities]); 
+    // fetches first25Cities plus capital
+  };
+
+  const handleSearchChange = event => {
+    const searchText = event.target.value;
+    const filtered = countries.filter(country => country.name.toLowerCase().includes(searchText.toLowerCase()));
+    setFilteredCountries(filtered);
+  };
+
+  return { countries: filteredCountries, selectedCountry, cities, capitalCity, temperature, handleCountryChange, cityArray, handleSearchChange };
+};
+
+export default useCitySelector;
+
