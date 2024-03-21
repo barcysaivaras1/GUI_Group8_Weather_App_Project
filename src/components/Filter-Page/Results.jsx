283fdda158d96a936fb5e8fd05f49c167139bb1d
barcysaivaras1 <barcysaivaras1@gmail.com>
2024-03-21 20:27:35 +0000

    Pushing Krish changes

--- src/components/Filter-Page/Results.jsx
+++ src/components/Filter-Page/Results.jsx
@@ -3,7 +3,7 @@ import './Results.css';
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import useNewWeatherData from "../Search-Page/NewWeatherHook";
 import Animate_page from "../../Animate-page";
-import SearchResults from "../Search-Page/results";
+import SearchResults from "../../components/Search-Page/results";
 
 export const ResultsPage = () =>{
     const cities = ["London","Kyoto","Dubai","Manchester"]
@@ -14,14 +14,15 @@ export const ResultsPage = () =>{
         }, [finalArray]); // Only re-run when finalArray changes
     const weatherData = useNewWeatherData(finalArray);
     console.log(weatherData[2])
+    console.log('redirected')
 
     const navigate = useNavigate();
 
-    const selected_city = "Manchester" //Hardcoded Manchester for now, use handleApplyClick as onClick in <SearchResults> so that it navigates to basepage_alt
-    const handleApplyClick = () => {
-        // const testArray = [1, 2, 3, 4, 5];  // Your test array
-        navigate("/", { state: {selected_city} }); //IMPORTANT: this sends selected_city to basepage_alt
-    }
+    const [selected_city,setSelectedCity] = useState("") //Hardcoded Manchester for now, use handleApplyClick as onClick in <SearchResults> so that it navigates to basepage_alt
+    const handleApplyClick = (cityName) => {
+        setSelectedCity(cityName);
+        navigate("/alt", { state: { selected_city: cityName } }); //IMPORTANT: this sends selected_city to basepage_alt
+    };
 
     return(
         <Animate_page>
@@ -30,7 +31,7 @@ export const ResultsPage = () =>{
             <span className="find-places-to-go">Find Places To Go</span>
             <div className="cont">
                 <div className="results-container">
-                    <button onClick = {handleApplyClick} >redirect to basepage_alt</button>
+                    {/* <button onClick = {handleApplyClick('London')} >redirect to basepage_alt</button> */}
                 {weatherData.map((data, index) => (
                 <SearchResults
                     key={index}
@@ -39,7 +40,10 @@ export const ResultsPage = () =>{
                     temp = {data.main && (
                         <p className='temp'>{Math.round(data.main.temp)}°C</p>
                         )}
-                    image = {`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="Weather Icon" />  
+                    image = {`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="Weather Icon" 
+                    onClick={() => handleApplyClick(data.name)} // Call handleApplyClick with city name onClick
+                    />  
+                    
             ))}
                 </div>
             </div>
